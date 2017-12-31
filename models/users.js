65e09960fd77235ad155f9b2051c9cdd1f071793
Lisa Jetton <ldjetton21@gmail.com>
2017-12-31 03:04:30 -0600

    updating/personalizing the rough draft

---
+++ models/users.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  memberType: type: { type: String, required: true },
+  date: { type: Date, default: Date.now }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
+//DB in mongoose, (Robo 3t), should be "mav2users".
