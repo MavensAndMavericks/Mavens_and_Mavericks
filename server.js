0fc81800635cdac4699013fef07935e5ecac9896
Lisa Jetton <ldjetton21@gmail.com>
2018-01-02 17:07:26 -0600

    added comment headers in server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//WEB DEPENDENCIES:
+//===============================================
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -5,35 +7,37 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 7700;
 
-require("./keys.js"); //IN order to import the "googleAuth" variable for the google auth keys (needed) below.
-
 const traitify = require('traitify');
 const Chart = require('chart.js');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
-// Use the GoogleStrategy within Passport (As shown - in the configuration - below.
+// Use the GoogleStrategy within Passport (As shown - in the configuration area - below.
 //   Strategies in passport require a `verify` function, which accept.
-//    ... credentials (in this case, a token, tokenSecret, and Google profile),
+//    ... credentials (in this case, a token, tokenSecret, and Google profile) -> which have been saved in keys.js,
 //    ... and invoke a callback with a user object.
 
 
+//LOCAL DEPENDENCY /IMPORTS
+//===============================================
+const keys = require("./keys.js"); //IN order to import the "googleAuth" variable for the google auth keys (needed) below. 
+
+
+
+//SERVER SET-UP (Routing MIDDLEWARE Definition)
+//=========================================================================================
 // Configure Body-Parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 // Serve up static assets
 app.use(express.static("client/build"));
-// Add routes, both API and view
+
+// Add routes, both API routes and view(html) routes
 app.use(routes);
 
 
-// Configure Traitify
-traitify.setHost("lvh.me");
-traitify.setVersion("v1");
-traitify.setSecretKey("fakeKey");
-
-
 // Configure Passport.js
-passport.use(new GoogleStrategy( googleAuth,
+passport.use(new GoogleStrategy( keys.googleAuth,  //would it be "googleAuth" or keys.googleAuth" to reference the keys here as there is ONLY ONE export?
   function(token, tokenSecret, profile, done) {
       User.findOrCreate({ googleId: profile.id }, function (err, user) {
         return done(err, user);
@@ -45,8 +49,17 @@ passport.use(new GoogleStrategy( googleAuth,
 // Configure Chart.js
 //const myChart = new Chart(ctx, {...});
 
-// Set up promises with mongoose
-mongoose.Promise = global.Promise;
+
+// Configure Traitify
+traitify.setHost("lvh.me");
+traitify.setVersion("v1");
+traitify.setSecretKey("fakeKey");
+
+
+
+//PORT and Database Definition AND Set-up
+//=========================================
+mongoose.Promise = global.Promise; // Set up promises with mongoose
 
 // Connect to the Mongo DB
 ////var MONGODB_URI = mongodb://heroku_p6dj1jmx:vdojlgvrsvdl6becgahtq7skel@ds237947.mlab.com:37947/heroku_p6dj1jmx
@@ -59,7 +72,9 @@ mongoose.connect(
 );
 
 
-// Start the API server
+
+// Start the Server // Initialize Server Listiner with PORT Connection..
+//==========================================================================
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
+}); // Starting the API server
