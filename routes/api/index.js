ef175af47fefe0781f70ab6f054665637cefd736
Lisa Jetton <ldjetton21@gmail.com>
2018-01-02 19:41:08 -0600

    added assessment to models, controller, and route directories

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,15 @@
 const router = require("express").Router();
 const bookRoutes = require("./books");
 const userRoutes = require("./users");
+const assessmentRoutes = require("./assessment");
 const oAuthRoutes = require("./oauth");
 
 // Book routes
 router.use("/books", bookRoutes);
 router.use("/users", userRoutes);
-router.use("/auth", oAuthRoutes)
+router.use("/assessment", assessmentRoutes);
+app.use("/auth", oAuthRoutes)
 
-module.exports = router;
+module.exports = {
+	router, app
+};
--- routes/api/oauth.js
+++ routes/api/oauth.js
@@ -1,14 +1,15 @@
-const router = require("express").Router();
+const express = require("express");
+const app = express();
 
-// GET /auth/google
-//   ... Usepassport.authenticate() as route middleware to authenticate the
+// GET /auth/google (BELOW)
+//   ... Use passport.authenticate() as route middleware to authenticate the
 //   ...  request.  The first step in Google authentication will involve redirecting
 //   ...  the user to google.com.  After authorization, Google will redirect the user
 //   ...  back to this application at /auth/google/callback
 app.get('/auth/google',
   passport.authenticate('google', { scope: 'https://www.google.com/m8/feeds' });
 
-// GET /auth/google/callback
+// GET /auth/google/callback (BELOW)
 //   Use passport.authenticate() as route middleware to authenticate the
 //   ...  request.  If authentication fails, the user will be redirected back to the
 //   ...  login page.  Otherwise, the primary route function function will be called,
@@ -19,4 +20,4 @@ app.get('/auth/google/callback',
     res.redirect('/');
   });
 
-module.exports = router;
+module.exports = app;
