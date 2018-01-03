2eb0d7fa825116a7fdaf77aa7c88f2c276d42720
Lisa Jetton <ldjetton21@gmail.com>
2018-01-03 15:07:16 -0600

    server.js require updates

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //WEB DEPENDENCIES:
 //===============================================
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
