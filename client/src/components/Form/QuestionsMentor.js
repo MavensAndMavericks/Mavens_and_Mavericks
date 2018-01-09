12ccf43192faa80a123e272e2ed5707caedc7eea
lorstevens <lornacstevens@gmail.com>
2018-01-08 19:33:49 -0600

    changes to css

--- client/src/components/Form/QuestionsMentor.js
+++ client/src/components/Form/QuestionsMentor.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./QuestionsMentor.css";
 
 export const QuestionsMentor = props =>
   // <div className="form-group">
@@ -8,64 +9,64 @@ export const QuestionsMentor = props =>
 <div>
         <div row>
             <h5>8. What is your current level of experience? </h5>
-            <input className="with-gap" name="group1" type="radio" id="q1" checked />
+            <input className="with-gap"  type="radio" id="q1"  />
             <label for="group1"> Novice</label>
 
-            <input className="with-gap" name="group1" type="radio" id="q1" checked />
+            <input className="with-gap"  type="radio" id="q1"  />
             <label for="group1"> College</label>
 
-            <input className="with-gap" name="group1" type="radio" id="q1" checked />
+            <input className="with-gap"  type="radio" id="q1"  />
             <label for="group1"> New Professional</label>
 
-            <input className="with-gap" name="group1" type="radio" id="q1" checked />
+            <input className="with-gap"  type="radio" id="q1"  />
             <label for="group1"> Professional 5+ Years</label>    
         </div>
 
         <div row>
             <h5>9. What are you preferred languages? </h5>
-            <input className="with-gap" name="group2" type="radio" id="q2" checked />
-            <label for="group2"> JavaScript</label>
+            <input className="with-gap" name="group2" type="checkbox" id="q2"  />
+            <label for="group2"> JavaScript </label>
 
-            <input className="with-gap" name="group2" type="radio" id="q2" checked />
-            <label for="group2"> Python</label>
+            <input className="with-gap" name="group2" type="checkbox" id="q2"  />
+            <label for="group2"> Python </label>
 
-            <input className="with-gap" name="group2" type="radio" id="q2" checked />
+            <input className="with-gap" name="group2" type="checkbox" id="q2"  />
             <label for="group2"> PHP</label>  
 
-            <input className="with-gap" name="group2" type="radio" id="q2" checked />
+            <input className="with-gap" name="group2" type="checkbox" id="q2"  />
             <label for="group2"> Ruby</label>   
 
-            <input className="with-gap" name="group2" type="radio" id="q2" checked />
+            <input className="with-gap" name="group2" type="checkbox" id="q2"  />
             <label for="group2"> C++</label>  
 
-            <input className="with-gap" name="group2" type="radio" id="q2" checked />
+            <input className="with-gap" name="group2" type="checkbox" id="q2"  />
             <label for="group2"> SQL</label> 
 
-            <input className="with-gap" name="group2" type="radio" id="q2" checked />
+            <input className="with-gap" name="group2" type="checkbox" id="q2"  />
             <label for="group2"> HTML/CSS</label> 
         </div>
 
         <div row>
             <h5>10. What is your industry of expertise? </h5>
-            <input className="with-gap" name="group3" type="radio" id="q3" checked />
+            <input className="with-gap" name="group3" type="checkbox" id="q3"  />
             <label for="group3"> Security</label>
 
-            <input className="with-gap" name="group3" type="radio" id="q3" checked />
+            <input className="with-gap" name="group3" type="checkbox" id="q3"  />
             <label for="group3"> Commerce</label>
 
-            <input className="with-gap" name="group3" type="radio" id="q3" checked />
+            <input className="with-gap" name="group3" type="checkbox" id="q3"  />
             <label for="group3"> Finance</label>  
 
-            <input className="with-gap" name="group3" type="radio" id="q3" checked />
+            <input className="with-gap" name="group3" type="checkbox" id="q3"  />
             <label for="group3"> Health</label>    
 
-            <input className="with-gap" name="group3" type="radio" id="q3" checked />
+            <input className="with-gap" name="group3" type="checkbox" id="q3"  />
             <label for="group3"> Gaming</label> 
 
-            <input className="with-gap" name="group3" type="radio" id="q3" checked />
+            <input className="with-gap" name="group3" type="checkbox" id="q3"  />
             <label for="group3"> Social Media</label> 
 
-            <input className="with-gap" name="group3" type="radio" id="q3" checked />
+            <input className="with-gap" name="group3" type="checkbox" id="q3"  />
             <label for="group3"> Marketing</label> 
         </div>
 
