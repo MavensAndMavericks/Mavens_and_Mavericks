a8afaa184f317a381dcb5f342a223cdaf8594b2e
lorstevens <lornacstevens@gmail.com>
2018-01-09 11:49:08 -0600

    changes to state on form

--- client/src/components/Form/QuestionsMentee.js
+++ client/src/components/Form/QuestionsMentee.js
@@ -5,16 +5,16 @@ export const QuestionsMentee = props =>
 <div>
         <div row>
             <h5>7. What is your current level of experience? </h5>
-            <input className="with-gap" name="group1" type="radio" id="q1" checked />
+            <input className="with-gap" name="group1" type="radio" id="q1" />
             <label for="group1"> Novice</label>
 
-            <input className="with-gap" name="group1" type="radio" id="q1" checked />
+            <input className="with-gap" name="group1" type="radio" id="q1" />
             <label for="group1"> College</label>
 
             <input className="with-gap" name="group1" type="radio" id="q1" checked />
             <label for="group1"> New Professional</label>
 
-            <input className="with-gap" name="group1" type="radio" id="q1" checked />
+            <input className="with-gap" name="group1" type="radio" id="q1" />
             <label for="group1"> Professional 5+ Years</label>    
         </div>
 
@@ -23,22 +23,22 @@ export const QuestionsMentee = props =>
             <input className="with-gap" name="group2" type="radio" id="q2" checked />
             <label for="group2"> JavaScript</label>
 
-            <input className="with-gap" name="group2" type="radio" id="q2" checked />
+            <input className="with-gap" name="group2" type="radio" id="q2"  />
             <label for="group2"> Python</label>
 
-            <input className="with-gap" name="group2" type="radio" id="q2" checked />
+            <input className="with-gap" name="group2" type="radio" id="q2"  />
             <label for="group2"> PHP</label>  
 
-            <input className="with-gap" name="group2" type="radio" id="q2" checked />
+            <input className="with-gap" name="group2" type="radio" id="q2"  />
             <label for="group2"> Ruby</label>   
 
-            <input className="with-gap" name="group2" type="radio" id="q2" checked />
+            <input className="with-gap" name="group2" type="radio" id="q2"  />
             <label for="group2"> C++</label>  
 
-            <input className="with-gap" name="group2" type="radio" id="q2" checked />
+            <input className="with-gap" name="group2" type="radio" id="q2"  />
             <label for="group2"> SQL</label> 
 
-            <input className="with-gap" name="group2" type="radio" id="q2" checked />
+            <input className="with-gap" name="group2" type="radio" id="q2"  />
             <label for="group2"> HTML/CSS</label> 
         </div>
 
@@ -47,22 +47,22 @@ export const QuestionsMentee = props =>
             <input className="with-gap" name="group3" type="radio" id="q3" checked />
             <label for="group3"> Security</label>
 
-            <input className="with-gap" name="group3" type="radio" id="q3" checked />
+            <input className="with-gap" name="group3" type="radio" id="q3"  />
             <label for="group3"> Commerce</label>
 
-            <input className="with-gap" name="group3" type="radio" id="q3" checked />
+            <input className="with-gap" name="group3" type="radio" id="q3"  />
             <label for="group3"> Finance</label>  
 
-            <input className="with-gap" name="group3" type="radio" id="q3" checked />
+            <input className="with-gap" name="group3" type="radio" id="q3"  />
             <label for="group3"> Health</label>    
 
-            <input className="with-gap" name="group3" type="radio" id="q3" checked />
+            <input className="with-gap" name="group3" type="radio" id="q3"  />
             <label for="group3"> Gaming</label> 
 
-            <input className="with-gap" name="group3" type="radio" id="q3" checked />
+            <input className="with-gap" name="group3" type="radio" id="q3"  />
             <label for="group3"> Social Media</label> 
 
-            <input className="with-gap" name="group3" type="radio" id="q3" checked />
+            <input className="with-gap" name="group3" type="radio" id="q3"  />
             <label for="group3"> Marketing</label> 
         </div>
 
